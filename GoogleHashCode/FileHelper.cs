e3d34ec202d53dc05f6278bc9ddf2d4fbff539f5
Carl Weilguny <carlweilguny@Carls-MacBook-Pro.local>
2018-03-01 17:48:09 +0100

    first commit 2

---
+++ GoogleHashCode/FileHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace GoogleHashCode
+{
+    public class FileHelper
+    {
+        public static string[] ReadInput(string path = "input.in")
+        {
+            return File.ReadAllText(path).Split(' ');
+        }
+
+        public static void WriteOutput(string output, string path="output.out")
+        {
+            File.WriteAllText(path, output);
+        }
+    }
+}
